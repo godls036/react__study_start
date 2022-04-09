c3ca3eed31f7a96b4d402de0eaa6eaca65bbfe55
unknown <godls036@gamil.com>
2022-04-10 01:10:32 +0900

    Add 7.TicTacToe

---
+++ 7.Tictactoe/Table.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tr from './Tr';
+
+
+const Table = ({ dispatch, tableData }) => {
+    return (
+        <table>
+            {Array(tableData.length).fill().map((tr, i) => (<Tr dispatch={dispatch} rowIndex={i} rowData={tableData[i]} />))}
+        </table>
+    );
+}
+
+export default Table;
+
